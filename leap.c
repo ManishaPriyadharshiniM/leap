6d981aca633713217411c79d77fd628cc1440de0
ManishaPriyadharshiniM <manisha96priya@gmail.com>
2016-07-08 12:27:31 +0530

    finish

---
+++ leap.c
@@ -0,0 +1,16 @@
+# leap
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int a;
+printf("enterthe year");
+scanf("%d",a);
+if(a%4==0)
+{
+printf("it is a leap year");
+else
+printf("it is not a leap year");
+}
+return 0;
+}
